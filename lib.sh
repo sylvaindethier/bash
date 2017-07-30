0ebe30317ea1e40fc46a0aea2dab0f20f7032a59
sylvaindethier <dethiersylvain@gmail.com>
2017-07-30 02:22:32 +0200

    feat: add lib structure

---
+++ lib.sh
@@ -0,0 +1,9 @@
+# source all files within 'lib' directory
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+LIB=${DIR}/lib/*
+for file in ${LIB} ; do
+  if [ -x "$file" ]; then
+    source "$file"
+  fi
+done
